6bc218cd11a1224d027380eed37245c6e0f52df3
Linus Wälti <linuswalti@gmail.com>
2021-06-09 10:30:13 +0200

    Copied content from https://getubrugg.wixsite.com/getubrugg

--- script.js
+++ script.js
@@ -6,5 +6,5 @@ document.addEventListener("DOMContentLoaded", function(){
 function getYear(){
   // source: https://stackoverflow.com/questions/6002254/get-the-current-year-in-javascript //
   var year = new Date().getFullYear();
-  document.getElementById('trainingYear').innerHTML = "Wenn du Interesse am Geräteturnen hast und einen Einblick in ein Training haben möchtest, dann melde dich für die Schnuppertrainings im September " + year + " an.";
+  document.getElementById('trainingYear').innerHTML += year;
 }
