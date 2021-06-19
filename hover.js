8795fd2adff8301d7924200cb188603bf8c70744
Linus Wälti <linuswalti@gmail.com>
2021-06-19 15:14:32 +0200

    Done automatic slideshow

--- hover.js
+++ hover.js
@@ -1,3 +1,6 @@
+// TODO: Removing the subpages
+// TODO: Slow the animation down 
+
 document.addEventListener("DOMContentLoaded", function(){
   // BOXES IN HEADER //
   document.getElementById('home').addEventListener("mouseover", hoverHeader);
@@ -18,6 +21,7 @@ document.addEventListener("DOMContentLoaded", function(){
   document.getElementById('training').addEventListener("mouseover", appear);
   document.getElementById('photos').addEventListener("mouseover", appear);
   document.getElementById('intern').addEventListener("mouseover", appear);
+  document.getElementById('contact').addEventListener("mouseover", appear)
 	// REMOVE SUBPAGES WHEN HOVERING AGAIN //
   var hidElements = document.getElementsByClassName('hid');
   for (i = 0; i<hidElements.length; i++){
@@ -35,17 +39,30 @@ function hoverOutHeader(){
   this.style.transition = "1.5s";
 }
 
-// function appear(){
-// 	let parent = this.parentNode;
-// 	var nodes = parent.querySelectorAll(".hid");
-// 	for (var i = 0; i < nodes.length; i++) {
-//     nodes[i].style.display = "block";
-// 	}
-// }
-//
-// function disappear(){
-// 	var nodes = document.querySelectorAll("a.hid");
-// 	for (var i = 0; i < nodes.length; i++) {
-//     nodes[i].style.display = "none";
-// 	}
-// }
+function appear(){
+  let parent = this.parentNode;
+  var nodes = parent.querySelectorAll(".hid");
+  document.querySelector(".homebtn").style.display = "none";
+  document.querySelector(".trainingbtn").style.display = "none";
+  document.querySelector(".photosbtn").style.display = "none";
+  document.querySelector(".internbtn").style.display = "none";
+  document.querySelector('.contactbtn').style.display = "none";
+  document.querySelector(".infobtn").style.display = "none";
+  parent.style.display = "flex";
+  for (var i = 0; i < nodes.length; i++) {
+    nodes[i].style.display = "flex";
+  }
+}
+
+function disappear(){
+	var nodes = document.querySelectorAll("a.hid");
+	for (var i = 0; i < nodes.length; i++) {
+    nodes[i].style.display = "none";
+	}
+  document.querySelector(".homebtn").style.display = "flex";
+  document.querySelector(".trainingbtn").style.display = "flex";
+  document.querySelector(".photosbtn").style.display = "flex";
+  document.querySelector(".internbtn").style.display = "flex";
+  document.querySelector('.contactbtn').style.display = "flex";
+  document.querySelector(".infobtn").style.display = "flex";
+}
