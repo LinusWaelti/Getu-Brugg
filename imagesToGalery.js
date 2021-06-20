bd7d65c9c17e384bf43a245f5a553c78ffbb6b52
eliine <66423714+eliine@users.noreply.github.com>
2021-06-20 19:50:19 +0200

    Fotos, Jahresprogramm
    -	Fotos eingefügt
    o	Noch nicht responsive
    -	Namen durch Ort des Fotos ersetzt
    o	Habe noch nicht alle Kategorienfotos
    -	Jahresprogramm eingetragen
    o	In CSS ganz unten text align left

--- imagesToGalery.js
+++ imagesToGalery.js
@@ -5,6 +5,12 @@ document.addEventListener("DOMContentloaded", function(){
   document.getElementById('2016').addEventListener("click", printImages);
 });
 
+
+
 function printImages(){
-  
+  for(i=1; i<=ANZAHL_ELEMENTE_IM_ORNDER; i++) {
+    var test = document.createElement ("img");
+    test.src = "images/IMG("+i+").png";
+    document.querySelector (".images").appendChild(test)
+  }
 }
