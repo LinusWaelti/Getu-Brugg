c4daa28503b8683e8c93c74e3a40c03f702b73c4
Linus Wälti <linuswalti@gmail.com>
2021-06-20 18:24:04 +0200

    Done some styling + added new files (galery.html + imagesToGalery.js)

---
+++ imagesToGalery.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentloaded", function(){
+  document.getElementById('2019').addEventListener("click", printImages);
+  document.getElementById('2018').addEventListener("click", printImages);
+  document.getElementById('2017').addEventListener("click", printImages);
+  document.getElementById('2016').addEventListener("click", printImages);
+});
+
+function printImages(){
+  
+}
