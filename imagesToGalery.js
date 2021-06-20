72cd405b89be4d92e5b1df94bed22c31a751e247
Linus Wälti <linuswalti@gmail.com>
2021-06-20 20:53:55 +0200

    working on galery

--- imagesToGalery.js
+++ imagesToGalery.js
@@ -1,16 +1,39 @@
-document.addEventListener("DOMContentloaded", function(){
-  document.getElementById('2019').addEventListener("click", printImages);
-  document.getElementById('2018').addEventListener("click", printImages);
-  document.getElementById('2017').addEventListener("click", printImages);
-  document.getElementById('2016').addEventListener("click", printImages);
-});
+function getYear(){
+  console.log("Hi");
+}
 
+function printImages2019(){
+  for(i=1; i<=ANZAHL_ELEMENTE_IM_ORNDER; i++) {
+    var test = document.createElement ("img");
+    test.src = "PFAD/IMG("+i+").png";
+    document.querySelector (".images").appendChild(test)
+  }
+  document.querySelector("h1").innerHTML += " 2019";
+}
 
+function printImages2018(){
+  for(i=1; i<=ANZAHL_ELEMENTE_IM_ORNDER; i++) {
+    var test = document.createElement ("img");
+    test.src = "PFAD/IMG("+i+").png";
+    document.querySelector (".images").appendChild(test)
+  }
+  document.querySelector("h1").innerHTML += " 2018";
+}
 
-function printImages(){
+function printImages2017(){
+  for(i=1; i<=ANZAHL_ELEMENTE_IM_ORNDER; i++) {
+    var test = document.createElement ("img");
+    test.src = "PFAD/IMG("+i+").png";
+    document.querySelector (".images").appendChild(test)
+  }
+  document.querySelector("h1").innerHTML += " 2017";
+}
+
+function printImages2016(){
   for(i=1; i<=ANZAHL_ELEMENTE_IM_ORNDER; i++) {
     var test = document.createElement ("img");
     test.src = "images/IMG("+i+").png";
     document.querySelector (".images").appendChild(test)
   }
+  document.querySelector("h1").innerHTML += " 2016";
 }
